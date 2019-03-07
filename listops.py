a5b0ae35d4d6f12d44d75929e6899cb82175d8b6
TuttleStripes <46587239+TuttleStripes@users.noreply.github.com>
2019-03-06 18:29:38 -0600

    Add files via upload

---
+++ listops.py
@@ -0,0 +1,26 @@
+def add(*args):
+  return sum(args)
+  
+def sub(*args):
+  dif = args[0]
+  for i in args[1:]:
+    dif -= i
+  return dif
+
+def mult(*args):
+  product = args[0]
+  for i in args[1:]:
+    product *= i
+  return product
+
+def div(*args):
+  quotient = args[0]
+  for i in args[1:]:
+    quotient /= i
+  return quotient
+
+
+def listFuncs(func, *lists): 
+  return map(func, *lists)
+
+  
